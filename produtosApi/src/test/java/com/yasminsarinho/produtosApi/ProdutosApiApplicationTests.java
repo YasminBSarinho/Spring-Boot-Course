ace7b6264fc881518ba10d84743f599f5b11fb46
yasminsarinho <yasmiinsainho@gmail.com>
2025-03-21 10:28:40 -0300

    first commit

---
+++ produtosApi/src/test/java/com/yasminsarinho/produtosApi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasminsarinho.produtosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
